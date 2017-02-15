45f3cfc6dbabfda3ea6a22b589780099ad8c324d
James Linnegar <james@linnegar.com>
2017-02-15 21:31:52 +0000

    storage and intent refactors

--- dev.js
+++ dev.js
@@ -1,12 +1,15 @@
 var TramService = require('./lib/TramService');
 
- new TramService("9400ZZMAABM", "9400ZZMABUR")
+var Promise = require("bluebird");
+
+
+ var t = new TramService()
+ 	.from("9400ZZMAABM")
+ 	.to("9400ZZMABUR")
  	.departingAt("12:30:00")
  	.departingOn("2017-02-14")
  	.getTrams()
     .then(function(trams){
     	console.log(trams);
     })
-    .error(function(error){
-    	console.log(error);
-    });
+ ;
--- spec/TramchesterSpec.js
+++ spec/TramchesterSpec.js
@@ -1,12 +1,27 @@
 describe("Tramchester", function() {
   var Tramchester = require('../lib/Tramchester');
+  var Storage = require("../lib/Storage");
 
   beforeEach(function() {
     tramchester = new Tramchester();
   });
 
-  it("should exist", function() {
-    expect(true).toBeTruthy();  
+  it("should handle a 'RetrieveHomeStationIntent' intent", function(){
+  	expect(typeof tramchester.intentHandlers.RetrieveHomeStationIntent).toEqual("function");
   });
+
+  it("should save a valid station", function(){
+  	var mockIntent = {};
+  	var mockSession = {};
+  	var mockResponse = {
+  		tell: function(){},
+  		ask: function(){}
+  	};
+	
+	tramchester.intentHandlers.RetrieveHomeStationIntent(mockIntent, mockSession, mockResponse);
+
+  	expect(true).toBeTruthy();
+  });
+
 
 });
